4b82e457e9964f8d708250a99893974110be74f1
Vadim <luckjantsev@rambler.ru>
2021-12-05 20:42:13 +0300

    BLANCHARD2

---
+++ js/gallery.js
@@ -0,0 +1,69 @@
+let createStatus=0;
+function createGallery(){
+  console.log("Создаю галерею")
+  if (createStatus==0){
+    createStatus=1;
+let gallerySlider = new Swiper(".gallery__right--content", {
+  pagination: {
+    type: 'fraction',
+    el: '.gallery__pagination--right',
+     },
+  navigation: {
+	  nextEl:'.gallery__btn--next',
+	  prevEl:'.gallery__btn--prev',  
+  },
+
+   
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerColumn:1,
+      spaceBetween: 0
+    },
+    576: {
+      slidesPerView: 2,
+      slidesPerColumn:2,
+      spaceBetween: 30
+    },
+
+    1200: {
+      slidesPerView:2,
+      slidesPerColumn:2,
+      spaceBetween: 50
+    },
+    10000: {
+      slidesPerView:3,
+      slidesPerColumn:2,
+      spaceBetween: 50
+    }
+  },
+  spaceBetween: 30,
+  a11y: {
+    prevSlideMessage: 'Предыдущий',
+    nextSlideMessage: 'Следующий',
+  }
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
+});
+createStatus=0;
+}
+else {
+  return
+}
+}
+window.addEventListener('resize',createGallery)
+createGallery();
+
+
+
+
+
+
+
